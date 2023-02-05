c7833095066aadcb8934bc43287ffecfe193841f
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2023-02-05 13:18:45 +0200

    add private and restricted routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,8 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from '../Layout/Layout';
 import { refreshUser } from 'redux/auth/operations';
 import { useAuth } from 'hooks';
+import { RestrictedRoute } from '../RestrictedRoute';
+import { PrivateRoute } from '../PrivateRoute';
 
 const HomePage = lazy(() => import('pages/Home'));
 const RegisterPage = lazy(() => import('pages/Register'));
@@ -24,9 +26,27 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
       </Route>
     </Routes>
   );
