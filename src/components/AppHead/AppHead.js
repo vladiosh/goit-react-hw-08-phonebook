eba5a0951807317ef4e5327c9bfdca37ac1677e5
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2023-02-06 14:45:37 +0200

    Update AppHead.js

--- src/components/AppHead/AppHead.js
+++ src/components/AppHead/AppHead.js
@@ -2,7 +2,7 @@ import { Navigation } from '../Navigation/Navigation';
 import { UserMenu } from '../UserMenu/UserMenu';
 import { AuthNav } from '../AuthNav/AuthNav';
 import { useAuth } from 'hooks';
-import { Header } from './AppBar.styled';
+
 import { AppBar, Toolbar } from '@mui/material';
 
 export const AppHead = () => {
